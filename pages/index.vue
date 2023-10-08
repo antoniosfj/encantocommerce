c5ff8361795602180876617c2956242b9da7c586
Antonio Fernandes <antonio.fernandes.bsi@gmail.com>
2023-10-08 03:23:53 -0300

    public images

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,10 @@ const slides = [
     url: 'https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80'
   },
   {
-    url: 'https://plus.unsplash.com/premium_photo-1683578622275-42552d90b5a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80'
+    url: '/img/bbs2.jpg'
+  },
+  {
+    url: '/img/bbs.jpg'
   },
 ]
 </script>
